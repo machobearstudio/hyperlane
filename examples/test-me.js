d4bc73995d38e0958a8da69957bd31c54a9038e2
Anton Chernikov <a.chernikov@financialcad.com>
2017-10-02 10:09:49 -0700

    Experiments

--- examples/test-me.js
+++ examples/test-me.js
@@ -1,5 +1,5 @@
 import fetch from 'node-fetch';
-import { message, get, set, not, and, or, lift } from '../src'
+import { message, get, set, not, and, or, lift } from '../src/sync'
 import log from './log'
 
 // Test data
@@ -9,12 +9,31 @@ const test = message.construct({ a: true, b: false }, state)
 
 const getGithub = lift(() => fetch('https://github.com/').then(res => res.text()))
 
+const all = (...handlers) => message.extend(input =>
+  handlers
+    .map(handler => handler(input))
+    .reduce((acc, next) => message.construct(
+      acc.data.concat([next.data]),
+      { ...acc.scope, ...next.scope }
+    ), message.construct([]))
+)
+
+const chain = (...handlers) => message.extend(input =>
+  handlers.reduce((prev, handler) => handler(prev), input)
+)
+
 // Lenses
 const summate = (x, y) => x + y
 const sum = lift(summate)
 
 // Test flows
-const testFlow = and(get('a'), get('b'))
+const testFlow = chain(
+  all(
+    and(get('a'), get('b')),
+    or(get('a'), get('b'))
+  ),
+  set('result', get(''))
+)
 
 log(test)
 log(testFlow(test))
