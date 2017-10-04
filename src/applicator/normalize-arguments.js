858d051fd22303c3c6ae314df47a7a0266ad2b7d
Anton Chernikov <a.chernikov@financialcad.com>
2017-10-04 14:36:25 -0700

    Cleanup

--- src/applicator/normalize-arguments.js
+++
@@ -1,7 +0,0 @@
-const identity = x => x
-
-const normalizeArguments = (arity, params) => (
-  arity ===0 || arity > params.length ? params.concat([identity]) : params
-)
-
-export default normalizeArguments
