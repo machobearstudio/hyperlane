e66112095ea3b0c1c8b78e3b0ccdb86a8f100f79
Anton Chernikov <a.chernikov@financialcad.com>
2017-11-03 16:48:07 -0700

    More flow simplifications in in order to support message bus transport

--- examples/bus.js
+++ examples/bus.js
@@ -1,114 +1,106 @@
-import polyMap from 'poly-map'
-import polyFilter from 'poly-filter'
+import map from 'poly-map'
+import filter from 'poly-filter'
 import event from 'simple-event'
 import uuid from 'uuid'
-import { configure, message, get, set, chain, uppercase, lift, map } from '../src'
+import { configure, message, get, set, chain, uppercase, lift } from '../src'
 import log from './log'
 
-const bus = event();
-bus.on = (type, func) => bus.subscribe(m => (m.type === type ? func(m.payload) : undefined))
-bus.send = (type, payload) => (payload !== undefined && bus.emit({ type, payload }))
-
-const emitter = type => payload => (payload !== undefined && bus.emit({ type, payload }))
-const tranceiver = (typeIn, handler, typeOut) => x => Promise.resolve(x).then(m => (m.type === typeIn ? handler(m.payload) : undefined)).then(emitter(typeOut))
-
-const buffer = elements => {
-  let values = polyMap(x => undefined, elements)
-
-  return input => {
-    polyMap((value, key) => { values[key] = value }, input)
-
-    if (values.filter(x => x === undefined).length === 0) {
-      const result = polyMap(x => x, values)
-      values = polyMap(x => undefined, elements)
-
-      return result
-    }
+class Bus {
+  constructor() {
+    this.event = event()
+    this.count = 0
   }
-}
 
-const apply = func => payload => func(...Object.values(payload))
+  get nextId() {
+    this.count++
+    return this.count
+  }
 
-const parallel = funcs => {
-  const launchType = `pl_${uuid.v4()}`
-  const resultType = `pj_${uuid.v4()}`
-
-  const reducer = buffer(funcs)
-
-  polyMap((func, index) => {
-    if (func.$class) {
-      bus.on(func.$class, x => bus.send(resultType, reducer({ [index]: x })))
-    }
-  }, funcs)
-
-  bus.on(launchType, input => {
-    const immediate = polyMap((func, index) => {
-      const output = func(input)
-      if (!func.$class && !(output instanceof Promise)) return output
-    }, funcs)
-
-    bus.send(resultType, reducer(immediate))
-  })
-
-  const f = emitter(launchType)
-  f.$class = resultType
-
-  return f
-}
-
-const sequential = funcs => {
-  const launchType = `s_${uuid.v4()}`
-
-  const resultType = funcs.reduce((requestType, func) => {
-    const responseType = func.$class || `s_${uuid.v4()}`
-    bus.on(requestType, x => Promise.resolve(x)
-      .then(func)
-      .then(response => bus.send(responseType, response))
+  on(type, func) {
+    const handler = (type === '*'
+      ? m => func(m.payload, m.type)
+      : m => (m.type === type ? func(m.payload, m.type) : undefined)
     )
 
-    return responseType
-  }, launchType)
+    this.event.subscribe(handler)
+  }
 
-  const f = emitter(launchType)
-  f.$class = resultType
-
-  return f
+  dispatch(type, payload) {
+    if (payload instanceof Promise) {
+      payload.then(x => this.event.emit({ type, payload: x }))
+    } else if (payload !== undefined) {
+      this.event.emit({ type, payload })
+    }
+  }
 }
 
-const forAll = func => {
-  const launchType = uuid.v4()
-  const joinType = uuid.v4()
-  const resultType = uuid.v4()
+const createEmitter = (func) => {
+  if (func.$class === 'Emitter') return func
 
-  const joinEmitter = emitter(joinType)
-  bus.on(launchType, items => {
-    const reducer = buffer(funcs)
-    bus.subscribe(tranceiver(joinType, reducer, resultType))
+  const type = bus.nextId
+  const emitter = payload => bus.dispatch(type, func(payload))
+  emitter.$class = 'Emitter'
+  emitter.$type = type
 
-    polyMap(item => joinEmitter(func(payload)))
-  })
-
-  const f = emitter(launchType)
-  f.$class = resultType
-
-  return f
+  return emitter
 }
+
+const buffer = (type, structure) => {
+  let values = map(x => undefined, structure)
+
+  return (key, value) => {
+    values[key] = value
+
+    if (Object.values(filter(x => x === undefined, values)).length === 0) {
+      bus.dispatch(type, values)
+    }
+  }
+}
+
+const bus = new Bus()
+bus.on('*', (data, type) => console.log(data, type))
+
+const sequential = ([start, ...rest]) => {
+  const sequence = createEmitter(start)
+
+  const resultType = rest
+    .map(createEmitter)
+    .reduce((prev, next) => {
+      bus.on(prev, next);
+      return next.$type
+    }, sequence.$type)
+
+  sequence.$type = resultType
+
+  return sequence
+}
+
+const parallel = funcs => {
+  const batch = map(createEmitter, funcs)
+  const mapper = createEmitter(x => { map(f => f(x), batch); return undefined })
+
+  const reducer = buffer(mapper.$type, batch)
+  map((x, key) => bus.on(x.$type, value => reducer(key, value)), batch)
+
+  mapper.$t = 'parallel'
+
+  return mapper
+}
+
+const apply = func => createEmitter(inputs => func.apply(undefined, inputs))
 
 configure({
-  transport: {
-    apply,
-    parallel,
-    sequential,
-    forAll
-  }
+  transport: { parallel, sequential, apply, forAll: () => {} }
 })
 
-const flow = get('test')
-bus.subscribe(log)
-
-// const flow = get('test')
-// const flow = apply(() => console.log(`wow ${test}`))
-
-flow({ test: 'wow' })
+const flow = set('DOOOOOGE', uppercase(get('test')))
 // console.log(flow)
+// const flow = sequential([
+//   parallel([
+//     sequential([x => x.test, x => String(x).toUpperCase()]),
+//     sequential([x => x.test, x => String(x).toLowerCase()]),
+//   ]),
+//   apply((...xs) => xs[0])
+// ])
 
+flow({ test: 'Wow,Doge' })
--- src/flow/index.js
+++ src/flow/index.js
@@ -2,13 +2,29 @@ import polyFilter from 'poly-filter'
 import pipe from 'function-pipe'
 import { extract, construct, collect, spread, applicator } from '../message'
 import { sequential, parallel, apply, forAll } from '../transport'
-import { fragment, resolver } from './fragment'
 
 const identity = x => x
 
-const log = x => { console.log('boooooo', x); return x }
+export const constant = x => () => construct(x)
 
-export const when = fragment((condition, yes, no) => input => {
+export const structure = items => sequential([
+  parallel(map(resolver, items)),
+  collect
+])
+
+export const resolver = predicate => {
+  if (typeof predicate === 'function') {
+    return predicate
+  }
+
+  if (typeof predicate !== 'object' || predicate === null) {
+    return constant(predicate)
+  }
+
+  return structure(predicate)
+}
+
+export const when = (condition, yes, no) => input => {
   const original = construct(input)
   const flow = sequential([
     condition,
@@ -16,33 +32,34 @@ export const when = fragment((condition, yes, no) => input => {
   ])
 
   return flow(original)
-})
+}
 
-export const map = fragment(func => sequential([
+export const map = func => sequential([
   spread,
   forAll(func),
   collect
-]))
+])
 
-export const filter = fragment(func => sequential([
+export const filter = func => sequential([
   spread,
   forAll(when(func, identity, () => undefined)),
   polyFilter(x => x !== undefined),
   collect
-]))
+])
 
-export const chain = fragment((...steps) => sequential(steps))
+export const chain = (...steps) => sequential(steps)
 
-export const all = fragment((...steps) => sequential([
+export const all = (...steps) => sequential([
   parallel(steps),
   collect
-]))
+])
 
-export const functionCall = func => fragment((...args) => sequential([
-  parallel(args.concat([identity])),
+export const functionCall = func => (...args) => sequential([
+  construct,
+  parallel(args.concat([identity]).map(resolver)),
   apply(func)
-]))
+])
 
 export const lift = pipe(applicator, functionCall)
 
-export const pass = fragment(x => input => x(input))
+export const pass = x => input => x(input)
