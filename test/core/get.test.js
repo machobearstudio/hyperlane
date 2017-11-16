80816f0648c68bb23e5e509afdf991b40f9d315e
Anton Chernikov <a.chernikov@financialcad.com>
2017-11-16 11:59:43 -0800

    Unit-tests

--- test/core/get.test.js
+++ test/core/get.test.js
@@ -10,33 +10,44 @@ scenario(
   ["acts as identity", get(''), identity]
 )
 
-const shallowProperty = { doge: 'wow' }
-const deepProperty = { doge: { such: { much: 'wow' } } }
-
 scenario(
-  "get(location)",
+  "get(location: string)",
 
   ["reads data by name", get('doge'),
-    given(data(shallowProperty), verify(dataIs('wow')))
+    given(data({ doge: 'wow' }), verify(scopeInvariant, dataIs('wow'))),
+    given(data({ doge: { such: { much: 'wow' } } }), verify(scopeInvariant, dataIs({ such: { much: 'wow' } })))
   ],
 
   ["reads scope by name", get('doge'),
-    given(scope(shallowProperty), verify(dataIs('wow')))
+    given(scope({ doge: 'wow' }), verify(scopeInvariant, dataIs('wow'))),
+    given(scope({ doge: { such: { much: 'wow' } } }), verify(scopeInvariant, dataIs({ such: { much: 'wow' } })))
   ],
 
   ["reads data by deep path", get('doge.such.much'),
-    given(data(deepProperty), verify(dataIs('wow')))
-  ],
-
-  ["reads data by deep path as array", get(['doge', 'such', 'much']),
-    given(data(deepProperty), verify(dataIs('wow')))
+    given(data({ doge: { such: { much: 'wow' } } }), verify(scopeInvariant, dataIs('wow')))
   ],
 
   ["reads scope by deep path", get('doge.such.much'),
-    given(scope(deepProperty), verify(dataIs('wow')))
+    given(scope({ doge: { such: { much: 'wow' } } }), verify(scopeInvariant, dataIs('wow')))
   ],
 
-  ["reads scope by deep path as array", get(['doge', 'such', 'much']),
-    given(scope(deepProperty), verify(dataIs('wow')))
+  ["returns undefined if no property found", get('nothing.like.this'),
+    given(anything, verify(scopeInvariant, dataIs(undefined))),
+  ]
+)
+
+scenario(
+  "get(location: array)",
+
+  ["reads data by deep path", get(['doge', 'such', 'much']),
+    given(data({ doge: { such: { much: 'wow' } } }), verify(scopeInvariant, dataIs('wow')))
   ],
+
+  ["reads scope by deep path", get(['doge', 'such', 'much']),
+    given(scope({ doge: { such: { much: 'wow' } } }), verify(scopeInvariant, dataIs('wow')))
+  ],
+
+  ["returns undefined if no property found", get(['nothing', 'like', 'this']),
+    given(anything, verify(scopeInvariant, dataIs(undefined))),
+  ]
 )
--- test/scenario.js
+++ test/scenario.js
@@ -3,11 +3,15 @@ import { describe } from 'mocha'
 
 const scenario = (description, ...cases) =>
   describe(description, function () {
-    let i
+    let i, j
     for (i = 0; i < cases.length; i++) {
-      const [comment, flow, requirement] = cases[i]
+      const [comment, flow, ...requirements] = cases[i]
+
       it(comment, function () {
-        return requirement(flow).then(result => expect(result).toEqual(true))
+        return Promise
+          .all(requirements.map(f => f(flow)))
+          .then(results => results.filter(x => x !== true))
+          .then(result => expect(result).toEqual([]))
       })
     }
   })
